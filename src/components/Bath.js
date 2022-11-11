9c4d20b836ec41d3321ca856e4ce6a3bc9331e73
Elizabeth Hollinger <hollingere15@gmail.com>
2022-11-11 20:30:29 +0000

    Update 6 files

--- src/components/Bath.js
+++ src/components/Bath.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Sink from "./Sink";
 
-export default function Bath() {
+export default function Bath(props) {
   return (
     <div>
-      <h2>Bath</h2>
+      <h2>Bath {props.size} </h2>
       <Sink />
     </div>
   );
